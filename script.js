f25e495569abd8905584de34e893bde1f2227058
abond421 <alex.bond.3797@gmail.com>
2020-02-15 14:26:16 +1100

    Added sketch function. Divs now stay coloured once hovered

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
+let newColour;
+let container = document.getElementById("container");
+
 function makeGrid(rows, columns) {
-    for (var i = 0; i < rows; i++) {
-        for (var j = 0; j < columns; j++) {
-            var div = document.createElement("div");
-            document.getElementById("container").appendChild(div);
-        }
-        var nextRow = document.createElement("br");
-        document.getElementById("container").appendChild(nextRow);
+  for (var i = 0; i < rows; i++) {
+    for (var j = 0; j < columns; j++) {
+      var div = document.createElement("div");
+      container.appendChild(div);
+      div.classList.add("row");
     }
+    var nextRow = document.createElement("br");
+    container.appendChild(nextRow);
+  }
+}
+
+function sketch(colour) {
+  const rowList = document.querySelectorAll(".row");
+  rowList.forEach(row => {
+    row.addEventListener("mouseover", e => {
+      e.target.style.background = '#000';
+    });
+  });
+}
+
+function randomColour() {
+  let r = Math.floor(Math.random() * 256);
+  let g = Math.floor(Math.random() * 256);
+  let b = Math.floor(Math.random() * 256);
+  newColour = "rgb(" + r + ", " + g + ", " + b + ")";
 }
 
 makeGrid(16, 16);
+sketch();
